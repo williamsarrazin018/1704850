80741ab11e3bfc22afb5d2124c1d21d273f366df
William Sarrazin <williamsarrazin@msn.com>
2018-08-24 14:11:27 -0500

    Atelier 1 : Question 2

--- app/src/main/java/ca/cours5b5/williamsarrazin/Parametres.java
+++ app/src/main/java/ca/cours5b5/williamsarrazin/Parametres.java
@@ -2,6 +2,8 @@ package ca.cours5b5.williamsarrazin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class Parametres extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
+        Log.d("MonEtiquette", "Bonjour!");
     }
 }
