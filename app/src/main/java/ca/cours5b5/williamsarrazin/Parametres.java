f37b77dc5569fb2b67a6497bebedb5b19229d00f
William Sarrazin <williamsarrazin@msn.com>
2018-08-27 15:44:11 -0500

    Atelier 2

--- app/src/main/java/ca/cours5b5/williamsarrazin/Parametres.java
+++ app/src/main/java/ca/cours5b5/williamsarrazin/Parametres.java
@@ -11,6 +11,9 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette", "Bonjour!");
+
+        Log.d("Bonjour",  this.getResources().getString(R.string.message_bienvenue) + this.getResources().getString(R.string.orientation));
+
+
     }
 }
