69f940978b00d5e4d3ca1fb2bfad37dbb21f7b0f
williamsarrazin018 <williamsarrazin@msn.com>
2018-11-08 20:51:12 -0500

    FAIT: -Ajout nouvekkes methodes controeur modele TODO: -Revider le code et tester

--- app/src/main/java/ca/cours5b5/williamsarrazin/controleurs/ControleurModeles.java
+++ app/src/main/java/ca/cours5b5/williamsarrazin/controleurs/ControleurModeles.java
@@ -1,5 +1,7 @@
 package ca.cours5b5.williamsarrazin.controleurs;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.Map;
 import ca.cours5b5.williamsarrazin.controleurs.interfaces.Fournisseur;
 import ca.cours5b5.williamsarrazin.controleurs.interfaces.ListenerGetModele;
 import ca.cours5b5.williamsarrazin.donnees.Disque;
+import ca.cours5b5.williamsarrazin.donnees.ListenerChargement;
 import ca.cours5b5.williamsarrazin.donnees.Serveur;
 import ca.cours5b5.williamsarrazin.donnees.SourceDeDonnees;
 import ca.cours5b5.williamsarrazin.exceptions.ErreurModele;
@@ -58,25 +61,21 @@ public final class ControleurModeles {
 
     static void getModele(String nomModele, ListenerGetModele listenerGetModele){
 
-        //TODO etape 5
         Modele modele = modelesEnMemoire.get(nomModele);
 
+        //Si pas de modele
         if(modele == null){
 
-            final String nomDuModele = nomModele;
-
-
-
-            modele =  chargerViaSequenceDeChargement(nomModele);
+            creerModeleEtChargerDonnees(nomModele, listenerGetModele);
 
         } else {
 
-
+            listenerGetModele.reagirAuModele(modele);
 
         }
     }
 
-
+/*
     private static Modele chargerViaSequenceDeChargement(final String nomModele){
 
         Modele modele = creerModeleSelonNom(nomModele);
@@ -98,7 +97,7 @@ public final class ControleurModeles {
 
         return modele;
     }
-
+*/
     public static void sauvegarderModele(String nomModele) throws ErreurModele {
 
         for(SourceDeDonnees source : listeDeSauvegardes){
@@ -110,17 +109,23 @@ public final class ControleurModeles {
     }
 
 
-    private static Modele creerModeleSelonNom(String nomModele) throws ErreurModele {
+    private static Modele creerModeleSelonNom(String nomModele, final ListenerGetModele listenerGetModele) throws ErreurModele {
 
         if(nomModele.equals(MParametres.class.getSimpleName())){
 
-            return new MParametres();
+            listenerGetModele.reagirAuModele( new MParametres() );
 
         }else if(nomModele.equals(MPartie.class.getSimpleName())){
 
-            MParametres mParametres = (MParametres) getModele(MParametres.class.getSimpleName());
+            getModele(MParametres.class.getSimpleName(), new ListenerGetModele() {
+                @Override
+                public void reagirAuModele(Modele modele) {
 
-            return new MPartie(mParametres.getParametresPartie().cloner());
+                    MParametres params = (MParametres) modele;
+                    //Nouveau modele avec params de nouvelle partie
+                    listenerGetModele.reagirAuModele(new MPartie(params.getParametresPartie().cloner()));
+                }
+            });
 
         }else{
 
@@ -155,5 +160,99 @@ public final class ControleurModeles {
         return chemin;
 
     }
+
+    private static void creerModeleEtChargerDonnees(final String nomModele, final ListenerGetModele listenerGetModele) {
+
+        creerModeleSelonNom(nomModele, new ListenerGetModele() {
+
+            @Override
+            public void reagirAuModele(Modele modele) {
+
+                //Mettre le modele en memoire, puis charger
+                modelesEnMemoire.put(nomModele, modele);
+
+                chargerDonnees(modele, nomModele, listenerGetModele);
+
+            }
+        });
+
+
+    }
+
+
+    private static void chargerDonnees(Modele modele, String nomModele, ListenerGetModele listenerGetModele) {
+
+        String save = getCheminSauvegarde(nomModele);
+
+        //Charger d'apres la sequence
+        chargementViaSequence(modele, save, listenerGetModele, 0);
+
+    }
+
+    private static void chargementViaSequence(Modele modele, String cheminDeSauvegarde, ListenerGetModele listenerGetModele, int indiceSourceCourante){
+
+
+        if( indiceSourceCourante < sequenceDeChargement.length ){
+
+            //Source suivante ou courante
+            chargementViaSourceCouranteOuSuivante(modele, cheminDeSauvegarde, listenerGetModele, indiceSourceCourante);
+
+        }else{
+
+            //On fini le chargement
+            terminerChargement(modele, listenerGetModele);
+
+        }
+
+    }
+
+    private static void chargementViaSourceCouranteOuSuivante(final Modele modele, final String cheminDeSauvegarde, final ListenerGetModele listenerGetModele, final int indiceSourceCourante) {
+
+        //Charger modele de la source courante
+        sequenceDeChargement[indiceSourceCourante].chargerModele(cheminDeSauvegarde, new ListenerChargement() {
+
+            @Override
+            public void reagirSucces(Map<String, Object> objetJson) {
+
+                terminerChargementAvecDonnees(objetJson, modele, listenerGetModele);
+
+            }
+
+            @Override
+            public void reagirErreur(Exception e) {
+
+                //Erreur, prochaine source
+                chargementViaSourceSuivante(modele, cheminDeSauvegarde, listenerGetModele, indiceSourceCourante);
+
+            }
+        });
+
+    }
+
+    private static void chargementViaSourceSuivante(Modele modele, String cheminDeSauvegarde, ListenerGetModele listenerGetModele, int indiceSourceCourante) {
+
+        //On charge avec la prochaine source dans la sequence
+        chargementViaSequence(modele, cheminDeSauvegarde, listenerGetModele, (indiceSourceCourante + 1));
+    }
+
+    private static void terminerChargementAvecDonnees(Map<String, Object> objetJson, Modele modele, ListenerGetModele listenerGetModele) {
+
+        //Obtenir donnees
+        modele.aPartirObjetJson(objetJson);
+
+        terminerChargement(modele, listenerGetModele);
+
+    }
+
+    private static void terminerChargement(Modele modele, ListenerGetModele listenerGetModele) {
+        Log.d("atelier12", "fin loading");
+
+        //Reagir aux modif du modele
+        listenerGetModele.reagirAuModele(modele);
+
+    }
+
+
+
 
 }
